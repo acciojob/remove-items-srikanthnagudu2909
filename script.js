7942ae4b15a25a71a7bac012f7f4388b68f5f479
srikanthnagudu2909 <nagudusrikanth@gmail.com>
2025-09-10 12:16:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const selectColor=document.getElementById("colorSelect")
+const select=document.getElementById("selectremove")
+select.addEventListener("click",removecolor)
+	
+
+function removecolor() {
+	const selectedIndex=selectColor.selectedIndex
+	//selectedoption.remove()
+	
+	selectColor.remove(selectedIndex)
+	
+	
+}
+
